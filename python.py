26932b3d9f5eca7bdf080bc8f3bbdbb7b8e1184b
shaktia <44256188+shaktia@users.noreply.github.com>
2025-05-18 15:30:21 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,9 @@
-print("welcome, to code world")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, World from Jenkins and Flask!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
