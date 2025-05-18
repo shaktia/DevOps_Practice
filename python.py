fb341541ab73a92a6a78cdfacb29036454ea5f92
shaktia <44256188+shaktia@users.noreply.github.com>
2025-05-18 17:06:00 +0530

    Update python.py

--- python.py
+++ python.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World from Jenkins and Flask!"
+    return "Hello, Shakti this is your first pipelne project"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
